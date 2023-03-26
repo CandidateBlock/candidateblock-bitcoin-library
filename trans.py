925f3e0a7062902995fdaf7c5b3d90eac01678a2
CandidateBlock <admin@candidateblock.com>
2023-03-26 18:37:44 +0100

    Refactored trans into class and serialization

--- trans.py
+++ trans.py
@@ -1,8 +1,11 @@
 import hashlib
+from typing import List, Union
 
 import ecdsa
 
 import candidateblock_bitcoin_library as cbl
+
+# https://en.bitcoin.it/wiki/Transaction
 
 OP_DUP = b'\x76'
 OP_HASH160 = b'\xa9'
@@ -11,22 +14,164 @@ OP_CHECKSIG = b'\xac'
 
 SIGHASH_ALL = b'\x01'
 
+SATS_PER_BTC = 100000000
+
 
 def var_int(num: int) -> bytes:
-    if num <= 0xfc:
+    if num < 0xfd:
         ret = num.to_bytes(length=1, byteorder='little', signed=False)
     elif num <= 0xffff:
         ret = b'\xfd' + num.to_bytes(length=2, byteorder='little', signed=False)
     elif num <= 0xffffffff:
         ret = b'\xfe' + num.to_bytes(length=4, byteorder='little', signed=False)
-    else:
+    elif num <= 0xffffffffffffffff:
         ret = b'\xff' + num.to_bytes(length=8, byteorder='little', signed=False)
+    else:
+        raise ValueError(f"integer too large (8-Bytes max): {num}")
+
     return ret
+
+
+def var_string(input: bytes) -> bytes:
+    return var_int(num=len(input)) + input
+
+
+def pay_to_pub_key_hash(address: bytes) -> bytes:
+    script_pub_key = bytes()
+    script_pub_key += OP_DUP
+    script_pub_key += OP_HASH160
+    script_pub_key += var_string(input=address)
+    script_pub_key += OP_EQUALVERIFY
+    script_pub_key += OP_CHECKSIG
+    return script_pub_key
+
+
+def pay_to_script_hash(address: bytes) -> bytes:
+    pass
+
+
+class Tx_in:
+    def __init__(self, prev_tx_out_hash: bytes, prev_tx_out_index: int, script_sig: bytes = None, sequence: int = 0xffffffff):
+        self.prev_tx_out_hash = prev_tx_out_hash
+        self.prev_tx_out_index = prev_tx_out_index
+        self.script_sig = script_sig
+        self.sequence = sequence
+
+    def serialization(self):
+        tx_bytes = bytes()
+        tx_bytes += self.prev_tx_out_hash.to_bytes(
+            length=32, byteorder='little', signed=False)
+        tx_bytes += self.prev_tx_out_index.to_bytes(
+            length=4, byteorder='little', signed=False)
+        if self.script_sig is None:
+            tx_bytes += var_int(num=0)
+        else:
+            tx_bytes += var_int(num=len(self.script_sig))
+            tx_bytes += self.script_sig
+        tx_bytes += self.sequence.to_bytes(length=4, byteorder='little', signed=False)
+        return tx_bytes
+
+
+class Tx_out:
+    def __init__(self, value_in_sats: int, script_pub_key: bytes = None):
+        self.value_in_sats = value_in_sats
+        self.script_pub_key = script_pub_key
+
+    def serialization(self):
+        tx_bytes = bytes()
+        tx_bytes += self.value_in_sats.to_bytes(length=8,
+                                                byteorder='little', signed=False)
+        if self.script_pub_key is None:
+            tx_bytes += var_int(num=0)
+        else:
+            tx_bytes += var_int(num=len(self.script_pub_key))
+            tx_bytes += self.script_pub_key
+        return tx_bytes
+
+
+class Tx:
+    def __init__(self, version: int = 1, has_witness: bool = False, tx_ins: List[Tx_in] = [], tx_outs: List[Tx_out] = [], witness: bytes = None, lock_time: int = 0):
+        self.version = version
+        self.has_witness = has_witness
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.witness = witness
+        self.lock_time = lock_time
+
+    def serialization(self):
+        tx_bytes = bytes()
+        if self.has_witness is True:
+            # TODO - Fix seg witness
+            return tx_bytes
+        # Version
+        tx_bytes += self.version.to_bytes(length=4, byteorder='little', signed=False)
+        # Inputs
+        if self.tx_ins:
+            num_inputs = len(self.tx_ins)
+        else:
+            num_inputs = 0
+        tx_bytes += var_int(num=num_inputs)
+        for tx_in in self.tx_ins:
+            tx_bytes += tx_in.serialization()
+        # Outputs
+        if self.tx_outs:
+            num_outputs = len(self.tx_outs)
+        else:
+            num_outputs = 0
+        tx_bytes += var_int(num=num_outputs)
+        for tx_out in self.tx_outs:
+            tx_bytes += tx_out.serialization()
+        # Locktime
+        tx_bytes += self.lock_time.to_bytes(length=4, byteorder='little', signed=False)
+        return tx_bytes
 
 
 if __name__ == "__main__":
     print("*" * 120 + "\n* Transaction\n" + "*" * 120)
 
+    tx = Tx()
+
+    # Create an Input
+    signature = bytes.fromhex(
+        "3045022100884d142d86652a3f47ba4746ec719bbfbd040a570b1deccbb6498c75c4ae24cb02204b9f039ff08df09cbe9f6addac960298cad530a863ea8f53982c09db8f6e3813")
+    # Uncompressed 0x04 = 512-Bits = 64-Bytes
+    pub_key = bytes.fromhex(
+        "0484ecc0d46f1918b30928fa0e4ed99f16a0fb4fde0735e7ade8416ab9fe423cc5412336376789d172787ec3457eee41c04f4938de5cc17b4a10fa336a8d752adf")
+    script_sig = bytes()
+    script_sig += var_int(num=len(signature + SIGHASH_ALL))
+    script_sig += signature
+    script_sig += SIGHASH_ALL
+    script_sig += var_int(num=len(pub_key))
+    script_sig += pub_key
+    tx_in = Tx_in(
+        prev_tx_out_hash=0x7957a35fe64f80d234d76d83a2a8f1a0d8149a41d81de548f0a65a8a999f6f18,
+        prev_tx_out_index=0,
+        script_sig=script_sig,
+        sequence=0xffffffff)
+    tx.tx_ins.append(tx_in)
+
+    # Create an Output 1
+    address = bytes.fromhex("ab68025513c3dbd2f7b92a94e0581f5d50f654e7")
+    tx_out = Tx_out(
+        value_in_sats=1500000,
+        script_pub_key=pay_to_pub_key_hash(address=address))
+    tx.tx_outs.append(tx_out)
+
+    # Create an Output 2
+    address = bytes.fromhex("7f9b1a7fb68d60c536c2fd8aeaa53a8f3cc025a8")
+    tx_out = Tx_out(
+        value_in_sats=8450000,
+        script_pub_key=pay_to_pub_key_hash(address=address))
+    tx.tx_outs.append(tx_out)
+
+    # Serialize the Transcation
+    print(tx.serialization().hex())
+
+    # Correct Hex
+    correct_hex = bytes.fromhex("0100000001186f9f998a5aa6f048e51dd8419a14d8a0f1a8a2836dd734d2804fe65fa35779000000008b483045022100884d142d86652a3f47ba4746ec719bbfbd040a570b1deccbb6498c75c4ae24cb02204b9f039ff08df09cbe9f6addac960298cad530a863ea8f53982c09db8f6e381301410484ecc0d46f1918b30928fa0e4ed99f16a0fb4fde0735e7ade8416ab9fe423cc5412336376789d172787ec3457eee41c04f4938de5cc17b4a10fa336a8d752adfffffffff0260e31600000000001976a914ab68025513c3dbd2f7b92a94e0581f5d50f654e788acd0ef8000000000001976a9147f9b1a7fb68d60c536c2fd8aeaa53a8f3cc025a888ac00000000")
+    assert tx.serialization() == correct_hex
+
+'''
     # Version
     version = int(1).to_bytes(length=4, byteorder='little', signed=False)
     # Input Count
@@ -177,6 +322,8 @@ if __name__ == "__main__":
 
     print(sparrow_signed == trans)
     pass
+'''
+
 
 '''
 LSig    47
